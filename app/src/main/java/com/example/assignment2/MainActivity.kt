c9951f561f9764780e960c5babe836929bf25c03
Eysar14 <eysar14@gmail.com>
2025-03-15 23:23:37 +0800

    Assignment2

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -14,19 +14,23 @@ class MainActivity : AppCompatActivity() {
 
         val edA : EditText = findViewById(R.id.edA)
         val edB : EditText = findViewById(R.id.edB)
+        val edC : EditText = findViewById(R.id.edC)
         val btnCount : Button = findViewById(R.id.btnCount)
         val tvResult : TextView = findViewById(R.id.tvResult)
 
         btnCount.setOnClickListener {
             val a = edA.text.toString().toDoubleOrNull()
             val b = edB.text.toString().toDoubleOrNull()
+            val c = edC.text.toString().toDoubleOrNull()
 
-            if (a != null && b != null) {
-                val c = sqrt(a * a + b * b)
-                tvResult.setText("Sisi miring (c) = $c")
-            } else {
-                tvResult.setText("Masukkan angka yang valid!")
+            val result = when {
+                a != null && b != null && c == null -> "Sisi miring (c) = ${sqrt(a * a + b * b)}"
+                a != null && c != null && b == null -> "Sisi siku-siku (b) = ${sqrt(c * c - a * a)}"
+                b != null && c != null && a == null -> "Sisi siku-siku (a) = ${sqrt(c * c - b * b)}"
+                else -> "Masukkan dua nilai untuk mencari yang ketiga!"
             }
+
+            tvResult.text = result
         }
     }
 }
